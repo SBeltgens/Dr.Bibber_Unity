10724acfaf1e7a3814836a80fcfc5d8d1a04294f
SBeltgens <sg.beltgens@student.avans.nl>
2026-03-31 12:36:58 +0200

    Avatar selectie
    Avatar word lokaal opgeslagen en is terug te zien in de applicatie

--- Assets/Scripts/AvatarSelecter.cs
+++ Assets/Scripts/AvatarSelecter.cs
@@ -1,12 +1,44 @@
+using System;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class AvatarSelecter : MonoBehaviour
 {
     [SerializeField] private Button selectButton;
-    public void toglleSelectButton()
+    [SerializeField] private Button foxButton;
+    [SerializeField] private Button racconButton;
+    [SerializeField] private Button wolfButton;
+
+    public void ToglleSelectButton()
     {
-            selectButton.gameObject.SetActive(true);
-        
+            selectButton.gameObject.SetActive(true);       
+    }
+
+
+
+public void ColorSelectedAvatar(int avatarIndex)
+    {
+        Color32 unselectedColor = new Color32(200, 200, 200, 160);
+        PlayerPrefs.SetInt("avatar", avatarIndex);
+        if (avatarIndex == 0)
+        {
+            
+            foxButton.GetComponent<Image>().color = Color.white;
+            racconButton.GetComponent<Image>().color = unselectedColor;
+            wolfButton.GetComponent<Image>().color = unselectedColor;
+        }
+        else if (avatarIndex == 1)
+        {
+                racconButton.GetComponent<Image>().color = Color.white;
+                foxButton.GetComponent<Image>().color = unselectedColor;
+                wolfButton.GetComponent<Image>().color = unselectedColor;
+        }
+        else if (avatarIndex == 2)
+        {
+            wolfButton.GetComponent<Image>().color = Color.white;
+            foxButton.GetComponent<Image>().color = unselectedColor;
+            racconButton.GetComponent<Image>().color = unselectedColor;
+        }
     }
 }
---
+++ Assets/Scripts/SetAvatar.cs
@@ -0,0 +1,19 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetAvatar : MonoBehaviour
+{
+    public Image avatarImage;
+    public Sprite[] avatars;
+    private void Start()
+    {
+        LoadAvatar();
+        
+    }
+    public void LoadAvatar()
+    { 
+        int avatar = PlayerPrefs.GetInt("avatar");
+        avatarImage.sprite = avatars[avatar];
+    }
+}
