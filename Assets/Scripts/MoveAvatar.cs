45895c95c0c93ff934c56c9d63e593a564e532f5
SBeltgens <sg.beltgens@student.avans.nl>
2026-04-01 12:57:56 +0200

    start minigame

---
+++ Assets/Scripts/MoveAvatar.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class MoveAvatar : MonoBehaviour
+{
+    public float speed = 20f;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        Debug.Log(rb.linearVelocity.x);
+        float moveInput = Input.GetAxis("Horizontal");
+
+        rb.linearVelocity = new Vector2(moveInput * speed, rb.linearVelocity.y);
+    }
+}
