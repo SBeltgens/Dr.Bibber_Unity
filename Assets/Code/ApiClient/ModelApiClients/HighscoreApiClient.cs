0c96582f37906b993226d52d3f5888e5149b5a6c
Ruubbb <rubenhovestadt@outlook.com>
2026-04-03 21:57:50 +0200

    Highscore Model + Apiclient

---
+++ Assets/Code/ApiClient/ModelApiClients/HighscoreApiClient.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class HighscoreApiClient : MonoBehaviour
+{
+    public WebClient webClient;
+
+    // POST methode om highscore te sturen
+    public async Awaitable<IWebRequestReponse> PostHighscore(Highscore highscore)
+    {
+        string route = "/highscores";
+        string data = JsonConvert.SerializeObject(highscore, JsonHelper.CamelCaseSettings);
+
+        IWebRequestReponse response = await webClient.SendPostRequest(route, data);
+        return ParseResponse(response);
+    }
+
+    // GET methode om highscore te krijgen
+    public async Awaitable<IWebRequestReponse> GetHighscores(string gameName)
+    {
+        string route = "/highscores"; 
+        IWebRequestReponse response = await webClient.SendGetRequest(route);
+
+        switch (response)
+        {
+            case WebRequestData<string> data:
+
+                List<Highscore> highscores = JsonConvert.DeserializeObject<List<Highscore>>(data.Data);
+                if (highscores.Count > 0)
+                {
+                    highscores.Sort((a, b) => a.score.CompareTo(b.score)); 
+                    return new WebRequestData<Highscore>(highscores[0]);
+                }
+                return null;
+
+            default:
+                return response;
+        }
+    }
+
+    private IWebRequestReponse ParseResponse(IWebRequestReponse response)
+    {
+        switch (response)
+        {
+            case WebRequestData<string> data:
+                Highscore hs = JsonConvert.DeserializeObject<Highscore>(data.Data);
+                return new WebRequestData<Highscore>(hs);
+            default:
+                return response;
+        }
+    }
+}
---
+++ Assets/Code/Models/Highscores.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+[Serializable]
+public class Highscore
+{
+    public string userId;
+    public string gameName;
+    public int score;
+    public string updatedAt;
+}
